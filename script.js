2ca3995fb44d498f6c43599b2593d176543f4eca
muzammilali5994 <muzammilali5994@outlook.com>
2026-04-03 14:36:52 +0500

    update code

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 const scene = new THREE.Scene();
+const boxarea = document.getElementById("box");
 
-const geomodel = new THREE.BoxGeometry(2,2,2);
-const material = new THREE.MeshBasicMaterial({color:"red"});
-
-const box = new THREE.Mesh(geomodel,material);
-
+const geomodel = new THREE.BoxGeometry(2, 2, 2);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+const box = new THREE.Mesh(geomodel, material);
 scene.add(box);
 
 const size = {
-    width:700,
-    height:500
-}
-const camera = new THREE.PerspectiveCamera(75, size.width / size.height, 0.1, 1000)
-camera.position.z=4;
-camera.position.x=2;
+    width: 700,
+    height: 500
+};
 
-scene.add(camera);
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height, 0.1, 1000);
+camera.position.set(1, 1, 5);
 
-const boxarea = document.getElementById("box");
+const control = new OrbitControls(camera, boxarea);
+control.enableDamping = true;
 
 const renderer = new THREE.WebGLRenderer({
-    canvas: boxarea // This connects the code to your <canvas id="box">
+    canvas: boxarea
 });
-renderer.setSize(size.width,size.height);
+renderer.setSize(size.width, size.height);
 
-renderer.render(scene,camera);
+// Ab OrbitControls sahi kaam karega
+
+
+const tick = () => {
+    control.update();
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(tick);
+}
+
+tick();
